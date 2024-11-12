e33e44ab70991e8c6f599576ec5beb22a33aabe1
Chen Bin <banchizhudao@gmail.com>
2024-11-12 13:07:54 +0800

    Remove b2FakeJointDef.

--- include/box2d/types.h
+++ include/box2d/types.h
@@ -914,42 +914,6 @@ typedef struct b2WheelJointDef
 /// @ingroup wheel_joint
 B2_API b2WheelJointDef b2DefaultWheelJointDef( void );
 
-/// Fake joint definition
-///
-/// A fake joint connect two bodies for personal purpose.
-/// @note This is for personal purpose
-/// @ingroup fake_joint
-typedef struct b2FakeJointDef
-{
-	/// The first attached body
-	b2BodyId bodyIdA;
-
-	/// The second attached body
-	b2BodyId bodyIdB;
-
-	/// The local anchor point relative to bodyA's origin
-	b2Vec2 localAnchorA;
-
-	/// The local anchor point relative to bodyB's origin
-	b2Vec2 localAnchorB;
-
-	/// The bodyB angle minus bodyA angle in the reference state (radians)
-	float referenceAngle;
-
-	/// Set this flag to true if the attached bodies should collide
-	bool collideConnected;
-
-	/// User data pointer
-	void* userData;
-
-	/// Used internally to detect a valid definition. DO NOT SET.
-	int32_t internalValue;
-} b2FakeJointDef;
-
-/// Use this to initialize your joint definition
-/// @ingroup fake_joint
-B2_API b2FakeJointDef b2DefaultFakeJointDef( void );
-
 /// The explosion definition is used to configure options for explosions. Explosions
 /// consider shape geometry when computing the impulse.
 /// @ingroup world
